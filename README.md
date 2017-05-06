074460306e17f2bf90fb2025404923d38e87702c
hades <i@mayuko.cn>
2017-05-06 18:05:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ARU表情
+
+[DEMO](http://demo.mayuko.cn/aru/)
+
+所有的表情均[由AS原文如此绘制](http://weibo.com/silenthiker?refer_flag=1001030001_&nick=AS%E5%8E%9F%E6%96%87%E5%A6%82%E6%AD%A4)
+
+
+
---
+++ load.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Author: Mayuko
+ * @Date:   2017-01-16 12:56:56
+ * @Last Modified by:   Mayuko
+ * @Last Modified time: 2017-01-16 13:30:54
+ */
+
+
+for ($i=0; $i < 281; $i++) { 
+	echo '<div class="col s6 m4 l3">
+			<div class="w">
+			<img src="http://ojuxjurwa.bkt.clouddn.com/aru ('.($i+1).').png" alt="">
+			</div>
+		  </div>';
+}
+?>
